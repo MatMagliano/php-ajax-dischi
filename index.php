5d9a52ea75d21da6db6a4f4b11e515e55062f945
Matteo Magliano <matteo.magliano01@gmail.com>
2020-02-25 19:50:05 +0100

    fine esercizio

--- index.php
+++ index.php
@@ -11,20 +11,26 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <link rel="stylesheet" href="dist/app.css">
+  <title>Spotify-php</title>
 </head>
 <body>
+  <header>
+    <div class="nav">
+      <img src="img/logo-small copia.svg" alt="">
+    </div>
+  </header>
   <main>
-    
+
     <div class="wrapper">
-      <?php foreach ($db as $faq) { ?>
-          <img src="<?php echo $faq['poster']; ?>" alt="">
-          <h2><?php echo $faq['title']; ?></h2>
-          <p><?php echo $faq['author']; ?></p>
-          <p><?php echo $faq['year']; ?></p>
-
-
-        <?php } ?>
+        <?php foreach ($db as $album) { ?>
+          <div class="album">
+            <img src="<?php echo $album['poster']; ?>" alt="">
+            <h4><?php echo $album['title']; ?></h4>
+            <p><?php echo $album['author']; ?></p>
+            <p><?php echo $album['year']; ?></p>
+          </div>
+          <?php } ?>
     </div>
   </main>
 </body>
--- server.php
+++ server.php
@@ -2,64 +2,76 @@
 
 $db = [
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'The Eminem Show',
+      'author' => 'Eminem',
+      'year' => 2002,
+      'poster' => 'https://img.discogs.com/jywq3nzhIQmbkYTas8PY_XUYWMM=/fit-in/600x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1301557-1519995538-6720.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'When Disaster Strikes...',
+      'author' => 'Busta Rhymes',
+      'year' => 1997,
+      'poster' => 'https://img.discogs.com/EgwJOOZZ7r1yt21IRqqABTbUGZU=/fit-in/500x491/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1246843-1371922381-1277.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
+      'title' => 'Back In Black',
+      'author' => 'AC/DC',
       'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'poster' => 'https://img.discogs.com/hjVtzok-FcDgxnJQw4q2RwqWxUM=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-400591-1537035829-1391.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'Monster',
+      'author' => 'R.E.M',
+      'year' => 1994,
+      'poster' => 'https://img.discogs.com/eiDPybm3GaFqXkw0DFs1EXnxXig=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1073044-1373585294-2883.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'Zombie',
+      'author' => 'The Cranberries',
+      'year' => 1994,
+      'poster' => 'https://img.discogs.com/ZvM9zTRe_7ED9X2TaOxdhF4zkkQ=/fit-in/599x513/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1888854-1250324114.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'Human',
+      'author' => 'Rag\'n\'Bone Man',
+      'year' => 2017,
+      'poster' => 'https://img.discogs.com/qsiu08p3D6MDRW7S4wXQ-gQvJrI=/fit-in/600x590/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-9803703-1534462199-9250.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
+      'title' => 'Domani Smetto',
+      'author' => 'Articolo 31',
       'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'poster' => 'https://img.discogs.com/9-8sc5bj7ismZWQHLme4ZjHC4vk=/fit-in/600x582/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2472577-1285957696.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'Le Onde',
+      'author' => 'Ludovico Einaudi',
+      'year' => 1996,
+      'poster' => 'https://img.discogs.com/ILPIzP6NQlyA2Bf4MgVV7FCUpco=/fit-in/599x586/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-997207-1230324129.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'Love In The Future',
+      'author' => 'John Legend',
+      'year' => 2013,
+      'poster' => 'https://img.discogs.com/HNM85SYgGa8wK2VgJqvbqRH5xl8=/fit-in/600x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5725033-1400966501-6048.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'Evolve',
+      'author' => 'Image Dragons',
+      'year' => 2017,
+      'poster' => 'https://img.discogs.com/NGObjk0gIIbrE6Ot3LbEET7H390=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10469087-1501265566-7000.jpeg.jpg'
+    ],
+    [
+      'title' => 'Slash',
+      'author' => 'Slash',
+      'year' => 2010,
+      'poster' => 'https://img.discogs.com/vuO11MxNzNwC7n0ph_eracuw1ME=/fit-in/451x451/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2235037-1271429942.jpeg.jpg'
+    ],
+    [
+      'title' => 'Ska-P',
+      'author' => 'Ska-P',
+      'year' => 1994,
+      'poster' => 'https://img.discogs.com/yMpRGhb-fh0v9U25BPZsByznpvM=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1339432-1361440334-7934.jpeg.jpg'
     ]
 ]
 
